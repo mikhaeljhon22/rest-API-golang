0de3413b12efe7e887f1d6e2570f811488f59e2b
mikhaeljhon22 <mikhaeljhon22@gmail.com>
2025-05-13 11:12:52 +0700

    create service CreateAcc

--- service/user_service.go
+++ service/user_service.go
@@ -11,29 +11,26 @@ type UserService interface {
 	FindUser(username string) (*model.Users, error)
 	UpdateUser(id uint, username string) error
 	DeleteUser(username string) error
+	CreateAcc(userNews *model.UserNews) error 
 }
 
 type userService struct {
 	repo repository.UserRepository
 }
 
-func NewUserService(repo repository.UserRepository) UserService {
+func NewUserService(repo repository.UserRepository) UserService{
 	return &userService{repo}
 }
 
-func (s *userService) CreateUser(user *model.Users) error {
-	err := s.repo.Create(user)
-	if err != nil {
-		return err
-	}
-	return nil 
+func (s *userService) CreateUser(user *model.Users) error{
+	return s.repo.Create(user)
 }
 
-func (s *userService) GetAllUsers() ([]model.Users, error) {
+func (s *userService) GetAllUsers() ([]model.Users, error){
 	return s.repo.FindAll()
 }
 
-func (s *userService) FindUser(username string) (*model.Users, error) {
+func (s *userService) FindUser(username string) (*model.Users, error){
 	return s.repo.FindByUsername(username)
 }
 
@@ -43,4 +40,7 @@ func (s *userService) UpdateUser(id uint, username string) error {
 
 func (s *userService) DeleteUser(username string) error {
 	return s.repo.DeleteByUsername(username)
+}
+func (s *userService) CreateAcc(userNews *model.UserNews) error {
+	return s.repo.CreateAcc(userNews)
 }
