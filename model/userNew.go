15e846cfd9252affba5d5141054848db383693e2
mikhaeljhon22 <mikhaeljhon22@gmail.com>
2025-06-06 14:16:47 +0700

    add relationaldatabase

--- model/userNew.go
+++ model/userNew.go
@@ -7,7 +7,11 @@ type UserNews struct {
     Name      string    `gorm:"not null"`
     Username  string    `gorm:"unique"`
     Email     *string   `gorm:"type:varchar(100)"`
-    Password  string    `gorm:"not null"`
+    Password  string    `gorm:"not null"` 
+    /*
+    AnotherID uint
+    Users Users `gorm:"foreignKey:AnotherID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+    */
     CreatedAt time.Time `gorm:"autoCreateTime"`
     UpdatedAT time.Time `gorm:"autoUpdateTime"`
 }
